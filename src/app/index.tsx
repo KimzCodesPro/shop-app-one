c8293fa368d2aff340bb16c72a4367b0e2e3c40e
Kareem Nour <kimz.codes@outlook.com>
2026-06-16 21:19:31 +0300

    feat: add Husky hooks for linting and type-checking; refactor index.tsx to include useEffect for state logging

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 const Index = () => {
-  const [state, setState] = useState();
-
   return (
     <View style={styles.container}>
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
