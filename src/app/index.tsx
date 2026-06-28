b66c661b1f3d933e88e73acae3de81d657798476
Kareem Nour <kimz.codes@outlook.com>
2026-06-28 10:36:15 +0300

    fix: update translation handling and restore icon component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,24 @@
+import Icon from "@/components/icons";
 import { Typography } from "@/components/ui/display";
 import { useTheme, useTranslation, useUserPreferences } from "@/hooks";
 import { Button, StyleSheet, View } from "react-native";
 
 const Index = () => {
   const theme = useTheme();
-  const { t } = useTranslation();
+  const { direction, t } = useTranslation();
   const { setTheme, setLanguage } = useUserPreferences();
+  const isRTL = direction === "rtl";
   return (
     <View style={styles.container}>
       <Typography variant="heroRegular" color={theme.background.overlay}>
         {t("welcome", { name: "ahmed", age: 25 })}
       </Typography>
-      {/* <Icon
-        name="chevron"
+      <Icon
+        name="eye"
         size={24}
         color={theme.background.overlay}
         isRTL={isRTL}
-      /> */}
+      />
       <Button title="Dark Mode" onPress={() => setTheme("dark")} />
       <Button title="Light Mode" onPress={() => setTheme("light")} />
       <Button title="English" onPress={() => setLanguage("en")} />
