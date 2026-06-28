3d8278123a7435c01407303de4d54d34fe5dcf0b
Kareem Nour <kimz.codes@outlook.com>
2026-06-28 10:34:27 +0300

    feat: add translation functionality and improve language handling; update dependencies

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,26 @@
 import { Typography } from "@/components/ui/display";
-import { useTheme, useUserPreferences } from "@/hooks";
+import { useTheme, useTranslation, useUserPreferences } from "@/hooks";
 import { Button, StyleSheet, View } from "react-native";
 
 const Index = () => {
   const theme = useTheme();
-  const { setTheme } = useUserPreferences();
-
+  const { t } = useTranslation();
+  const { setTheme, setLanguage } = useUserPreferences();
   return (
     <View style={styles.container}>
       <Typography variant="heroRegular" color={theme.background.overlay}>
-        كريم نور امام
+        {t("welcome", { name: "ahmed", age: 25 })}
       </Typography>
+      {/* <Icon
+        name="chevron"
+        size={24}
+        color={theme.background.overlay}
+        isRTL={isRTL}
+      /> */}
       <Button title="Dark Mode" onPress={() => setTheme("dark")} />
       <Button title="Light Mode" onPress={() => setTheme("light")} />
+      <Button title="English" onPress={() => setLanguage("en")} />
+      <Button title="Arabic" onPress={() => setLanguage("ar")} />
     </View>
   );
 };
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useFontStyles } from "./useFontStyles";
 export { default as useTheme } from "./useTheme";
+export { default as useTranslation } from "./useTranslation";
 export { default as useUserPreferences } from "./useUserPreferences";
