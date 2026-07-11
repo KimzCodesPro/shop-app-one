d95902e273b1e18ec658a9206da32787858dd3e1
Kareem Nour <kimz.codes@outlook.com>
2026-07-11 15:15:55 +0300

    feat: add TopBar component with navigation support and integrate into Index page

---
+++ src/components/ui/navigations/TopBar/TopBar.tsx
@@ -0,0 +1,68 @@
+import Icon, { IconName } from "@/components/icons";
+import { Href, useRouter } from "expo-router";
+import { Pressable, StyleProp, View, ViewStyle } from "react-native";
+import { Typography } from "../../display";
+import useStyles from "./useStyles";
+
+type TopBarBaseProps = {
+  title: string;
+  actionBtn?: { iconName: IconName; onPress: () => void; flipOnRTL: boolean };
+  style?: StyleProp<ViewStyle>;
+};
+
+type TopBarBackProps =
+  | { goBack?: false; forceBack?: never }
+  | { goBack: true; forceBack?: Href };
+
+type TopBarProps = TopBarBaseProps & TopBarBackProps;
+
+const TopBar = ({ title, goBack, forceBack, actionBtn }: TopBarProps) => {
+  const router = useRouter();
+  const { styles, colors } = useStyles();
+
+  const hasForceBack = !!forceBack;
+
+  const handleGoBack = () => {
+    if (forceBack) {
+      router.push(forceBack);
+    } else {
+      if (router.canGoBack()) {
+        router.back();
+      }
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {goBack || hasForceBack ? (
+        <Pressable onPress={handleGoBack}>
+          <Icon name="chevron" size={22} color={colors.fg.primary} flipOnRTL />
+        </Pressable>
+      ) : (
+        <View />
+      )}
+
+      <Typography
+        variant="mediumBold"
+        color={colors.fg.primary}
+        textTransform="capitalize"
+      >
+        {title}
+      </Typography>
+      {actionBtn ? (
+        <Pressable onPress={actionBtn.onPress}>
+          <Icon
+            name={actionBtn.iconName}
+            size={22}
+            color={colors.fg.primary}
+            flipOnRTL={actionBtn?.flipOnRTL}
+          />
+        </Pressable>
+      ) : (
+        <View />
+      )}
+    </View>
+  );
+};
+
+export default TopBar;
